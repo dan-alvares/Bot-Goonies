05b9ae6a953f824e1286a74262ee32b11d534607
Dan Alvares <dan.alvares@gmail.com>
2023-11-07 07:38:09 -0300

    Correção requeriments
    Correções e ajustes para deploy do bot

--- goonies.py
+++ goonies.py
@@ -1,6 +1,7 @@
 import discord
-from discord import Embed, File
-from discord.ext import tasks, commands
+from discord import Embed
+from discord.ext import tasks
+import datetime
 from bot_info import botsecret
 from scrape_goon import scrape_goons_raid
 
@@ -21,7 +22,7 @@ class MyClient(discord.Client):
 
     @tasks.loop(seconds=300)  # Task é executada a cada 5 minutos
     async def my_background_task(self):
-        channel = self.get_channel(1161828992102432808)  # ID do canal onde a msg será enviada ID do canal: 1161828992102432808
+        channel = self.get_channel(1171087824678821959)  # ID do canal onde a msg será enviada ID do canal: 1161828992102432808
         obtem_info_goons = scrape_goons_raid()
         if self.raid_atual != obtem_info_goons[0] or self.horario_atual != obtem_info_goons[2]:
             self.raid_atual = obtem_info_goons[0]
@@ -47,6 +48,7 @@ class MyClient(discord.Client):
             embed_goons.add_field(name='Data', value=self.data_atual, inline=True)
             embed_goons.add_field(name='Horário', value=self.horario_atual, inline=True)            
             embed_goons.set_footer(text='Escape from Tarkov Brasil', icon_url='https://cdn.discordapp.com/attachments/1166543939898195979/1166544051219210260/EFT-BR_LOGO_2.png?ex=65541a4f&is=6541a54f&hm=8dd4f02d722b563a0aea69608e4182ac56907c7cc709e19eba3eb871e80dc532&')
+            print(f'{datetime.now()} - {self.raid_atual} - {self.data_atual} - {self.horario_atual}')
             
             await channel.send(embed=embed_goons) # envia a mensagem no canal com o mapa atual dos goons      
         
