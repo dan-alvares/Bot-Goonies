d78d659ceca034f7258d9807db5ecc2128112653
Dan Alvares <dan.alvares@gmail.com>
2023-11-08 08:18:17 -0300

    Atualização
    Ajustes nas thumbs, cores da embed e texto sobre horário

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import discord
 from discord import Embed
 from discord.ext import tasks
-import datetime
 from bot_info import botsecret
 from scrape_goon import scrape_goons_raid
 
@@ -30,23 +29,23 @@ class MyClient(discord.Client):
             self.horario_atual = obtem_info_goons[2]            
 
             if self.raid_atual == 'Customs':
-                imagem_thumb = 'https://media.discordapp.net/attachments/1166543939898195979/1171169676122927165/customs_thumb.jpg?ex=655bb3c2&is=65493ec2&hm=8f3dc555d25c5b5dc9cccd2b66cb24417cc6975806454bcdafbde5ee4e1061a6&='
+                imagem_thumb = 'https://cdn.discordapp.com/attachments/1166543939898195979/1171767722191884308/customs2.jpg?ex=655de0bb&is=654b6bbb&hm=a677579f5a69b7e7194438b545562c93f9e7693b5067c9c84d5dc717705e3cbe&'
                 cor_thumb = 15548997
             elif self.raid_atual == 'Lighthouse':
-                imagem_thumb = 'https://media.discordapp.net/attachments/1166543939898195979/1171169676445892608/lighthouse_thumb.jpg?ex=655bb3c2&is=65493ec2&hm=0e08f34f61960cd352a6c2f89eb923129c1e250f260c0192df32f124b7ec7aef&='
+                imagem_thumb = 'https://cdn.discordapp.com/attachments/1166543939898195979/1171767722468704396/lighthouse2.jpg?ex=655de0bb&is=654b6bbb&hm=345e24245b756ba2b76cf610e3f960f096e61def238907ad8bab1ce75abb0396&'
                 cor_thumb = 16776960
             elif self.raid_atual == 'Woods':
-                imagem_thumb = 'https://media.discordapp.net/attachments/1166543939898195979/1171169677012119604/woods_thumb.jpg?ex=655bb3c2&is=65493ec2&hm=c56eefd96f5479529d8948e9255db60577c6f6cff20329b10a088a5801416e0e&='
+                imagem_thumb = 'https://cdn.discordapp.com/attachments/1166543939898195979/1171767721898278982/woods2.jpg?ex=655de0bb&is=654b6bbb&hm=65e2c9ee37120976c68e116f98feb830abcdec71fa30669a21a832372c4cfb89&'
                 cor_thumb = 5763719
             elif self.raid_atual == 'Shoreline':
-                imagem_thumb = 'https://media.discordapp.net/attachments/1166543939898195979/1171169676726894622/shoreline_thumb.jpg?ex=655bb3c2&is=65493ec2&hm=573c8bc890044256da1ce4d311f1a640459ec0afdc6f786e1c12be8c9fcd48d3&='
-                cor_thumb = 15105570
+                imagem_thumb = 'https://cdn.discordapp.com/attachments/1166543939898195979/1171767722716172371/shoreline2.jpg?ex=655de0bb&is=654b6bbb&hm=bb29c3759070e218ee59168916956a7410661cc369922dee581e4ec0ea7fb539&'
+                cor_thumb = 1752220
 
             embed_goons = Embed(title='Goons Tracker', color=cor_thumb)
             embed_goons.set_image(url=imagem_thumb)
             embed_goons.add_field(name='Raid', value=self.raid_atual, inline=False)
             embed_goons.add_field(name='Data', value=self.data_atual, inline=True)
-            embed_goons.add_field(name='Horário', value=self.horario_atual, inline=True)            
+            embed_goons.add_field(name='Horário', value=f'{self.horario_atual} (horário de Brasília)', inline=True)            
             embed_goons.set_footer(text='Escape from Tarkov Brasil', icon_url='https://cdn.discordapp.com/attachments/1166543939898195979/1166544051219210260/EFT-BR_LOGO_2.png?ex=65541a4f&is=6541a54f&hm=8dd4f02d722b563a0aea69608e4182ac56907c7cc709e19eba3eb871e80dc532&')
             print(f'{self.raid_atual} - {self.data_atual} - {self.horario_atual}')
             
